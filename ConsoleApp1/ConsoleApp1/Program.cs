2c94de2169117e673ad2038e1466c87f1936a5c0
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2020-04-27 10:20:12 -0600

    added the console app solution.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message = "Jeremiah";
+            String message1 = "Idaho";
+
+            double width, height, woodLength, glassArea;
+
+            double Jeremiah, Idaho;
+
+            string widthString, heightString;
+
+            string Jeremiahstring, IdahoString;
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
+            Console.WriteLine("My name is " + message + ", and I am from " + message1);
+        }
+    }
+}
