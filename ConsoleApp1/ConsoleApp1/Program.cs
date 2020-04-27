14586253b50dc075e91436de8392472f9b4aff49
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2020-04-27 10:34:56 -0600

    entered other date portion.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,31 +12,37 @@ namespace ConsoleApp1
             Console.WriteLine("My name is " + message + ", and I am from " + message1);
             Console.ReadLine();
 
+            DateTime today = DateTime.Today;
+            Console.WriteLine("Today is " + today);
+            Console.ReadLine();
+
             DateTime date1 = new DateTime(2020, 12, 25);
             DateTime date2 = DateTime.Today;
             int daysDiff = ((TimeSpan)(date2 - date1)).Days;
+            Console.WriteLine("Days until Christmas: " + daysDiff);
+            Console.ReadLine();
+
+
         }
 
-        private static void GlazerApp()
-        {
+        private static void GlazerApp() { 
+        
+            double width, height, woodLength, glassArea; 
             string widthString, heightString;
 
-            string Jeremiahstring, IdahoString;
-
-            Console.WriteLine("Type the width (feet): ");
-            widthString = Console.ReadLine();
+            widthString = Console.ReadLine();   
             width = double.Parse(widthString);
 
-            Console.WriteLine("Type the height (meters): ");
-            heightString = Console.ReadLine();
+            heightString = Console.ReadLine();   
             height = double.Parse(heightString);
 
-            woodLength = 2 * (width + height) * 3.25;
-            glassArea = 2 * (width * height);
-
-            Console.WriteLine("The length of the wood is " + woodLength + " feet");
-            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            woodLength = 2 * (width + height ) * 3.25 ; 
+ 
+            glassArea = 2 * (width* height ) ; 
+ 
+            Console.WriteLine( "The length of the wood is " +     woodLength + " feet" ) ;
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres"); 
             Console.ReadLine();
         }
-    }
+}
 }
